26f1a1acacfa0d1f19308f4b92f351acf9f1f5cc
cat-in-pants <thedimbear@gmail.com>
2026-02-16 20:03:18 +0800

    Update connection.py

--- МДК.01.01 - Разработка программных модулей/Лабораторные и Практические работы/ЛАБ_1/task_manager/db/connection.py
+++ МДК.01.01 - Разработка программных модулей/Лабораторные и Практические работы/ЛАБ_1/task_manager/db/connection.py
@@ -1,1 +1,6 @@
+from sqlalchemy import create_engine
 
+def get_connection():
+    engine = create_engine('postgresql+psycopg2://postgres:password@localhost:5432/task_manager_db')
+    return engine
+)
